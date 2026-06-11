ea2a5f409be1d7bdc5058799b2fe279cda0c7116
annal <annal@Annalisa>
2026-06-11 22:21:03 +0200

    bozza inserimento articoli in ListaDiArticoli

--- src/main/java/model/ListaDiArticoli.java
+++ src/main/java/model/ListaDiArticoli.java
@@ -1,5 +1,66 @@
 package model;
 
+import java.util.Objects;
+
+/**
+ * Classe che implementa la gestione di una lista di articoli da acquistare (es.
+ * Lista Esselunga, Lista Ikea). Gestisce sia gli articoli attivi da comprare
+ * che quelli cancellati permettendone il ripristino. Ciascuna lista non può
+ * avere il nome uguale a un'altra lista.
+ * 
+ * @author Gibello
+ */
+
 public class ListaDiArticoli {
+
+	private final String nome;
+
+	/**
+	 * Costruttore di ListaDiArticoli.class. Esegue il controllo sul nome inserito
+	 * per la lista e blocca la creazione dell'oggetto se trova un inserimento null
+	 * o vuoto.
+	 * 
+	 * @param nome Il nome della lista.
+	 * @throws IllegalArgumentException Se il nome è nullo o vuoto.
+	 * 
+	 */
+
+	public ListaDiArticoli(String nome) {
+
+		if (nome == null || nome.isEmpty()) {
+
+			throw new IllegalArgumentException(
+					"Il nome assegnato alla lista di articoli non deve essere nullo o vuoto.");
+		}
+
+		this.nome = nome;
+	}
+
+	/**
+	 * 
+	 * @return Il nome della lista.
+	 */
+
+	public String getNome() {
+
+		return nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ListaDiArticoli other = (ListaDiArticoli) obj;
+		return Objects.equals(nome, other.nome);
+	}
 
 }
--- src/main/java/model/test/ListaDiArticoliTest.java
+++ src/main/java/model/test/ListaDiArticoliTest.java
@@ -1,14 +1,44 @@
-package test_model;
+package model.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import model.ListaDiArticoli;
+
+/**
+ * Classe di test per la verifica del funzionamento della classe
+ * ListaDiArticoli.
+ * 
+ * Verifica che il nome della lista venga salvato correttamente in seguito ad un
+ * inserimento valido. Effettua i controlli del lancio eccezioni in caso di
+ * inserimenti nulli o vuoti.
+ * 
+ * 
+ * @author Gibello
+ */
+
 class ListaDiArticoliTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testValidInsertion() {
+
+		ListaDiArticoli nome = new ListaDiArticoli("Spesa Ikea");
+
+		assertEquals("Spesa Ikea", nome.getNome());
+
+	}
+
+	@Test
+	void testNullInsertionThrowsException() {
+
+		assertThrows(IllegalArgumentException.class, () -> new ListaDiArticoli(null));
+	}
+
+	@Test
+	void testEmptyInsertionThrowsException() {
+
+		assertThrows(IllegalArgumentException.class, () -> new ListaDiArticoli(""));
 	}
 
 }
